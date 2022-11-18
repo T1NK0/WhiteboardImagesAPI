a59507473c6203b684f7beaa6d438b40423eb1f7
Mads Tinko Madsen <mtm@itklyngen.dk>
2022-11-18 10:55:12 +0100

    Updated API to post as well.

--- WhiteboardImagesAPI/Controllers/ImageController.cs
+++ WhiteboardImagesAPI/Controllers/ImageController.cs
@@ -3,20 +3,44 @@ using Microsoft.AspNetCore.Mvc;
 namespace WhiteboardImagesAPI.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("[controller]/[action]")]
     public class ImageController : ControllerBase
     {
+        private static List<string> _images = new List<string>();
 
-        [HttpGet(Name = "getImages")]
+        public ImageController()
+        {
+            if (_images.Count == 0)
+            {
+                var files = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + @"Images\");
+
+                foreach (var file in files)
+                {
+                    byte[] imageFileToByteArray = System.IO.File.ReadAllBytes(file);
+                    string base64FileToString = Convert.ToBase64String(imageFileToByteArray);
+                    _images.Add(base64FileToString);
+                }
+            }
+        }
+
+        [HttpGet(Name = "get")]
         public string GetImg()
         {
-            //C:\Users\mtm\source\repos\WhiteboardImagesAPI\WhiteboardImagesAPI\Images\
-            var files = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + @"Images\");
-            var random = new Random().Next(0, files.Length-1);
-            byte[] imageArray = System.IO.File.ReadAllBytes(files[random]);
-            string base64ImageRepresentation = Convert.ToBase64String(imageArray);
+            var random = new Random().Next(0, _images.Count-1);
+            string randomImageBase64 = _images[random];
+
             Console.WriteLine("Api is sending image " + random.ToString());
-            return base64ImageRepresentation;
+            return randomImageBase64;
+
         }
+
+        [HttpPost(Name = "save")]
+        public IActionResult SaveImg([FromForm]string image)
+        {;
+            _images.Add(image);
+            Console.WriteLine("Api saved image");
+            return Ok();
+        }
+
     }
 }
